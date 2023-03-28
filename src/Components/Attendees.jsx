6dd46c6fd94a48f6afb4854610238c5c597b3c36
Maja Gedda <maja.gedda@zocom.se>
2023-03-28 10:28:25 +0200

    fetch attendees

--- src/Components/Attendees.jsx
+++ src/Components/Attendees.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 function Attendees() {
+
+    useEffect(() => {
+        // vad som skall hända vid mount
+        fetch('https://majazocom.github.io/Data/attendees.json')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+    }, []);
+
     return ( 
         <section className="attendeesContainer">
             <h2>Här är de</h2>
